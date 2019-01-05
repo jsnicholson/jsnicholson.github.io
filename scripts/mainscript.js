f027a8863c26c1129960d32509dd2c2c9b6b45bf
Josh Nicholson <jsnicholson08@gmail.com>
2019-01-05 23:38:32 +0000

    Major changes
    implemented content pane
    project cards
    navbar

---
+++ scripts/mainscript.js
@@ -0,0 +1,4 @@
+function readTextFile(filePath) {
+	//document.getElementById("contentContainer").innerHTML = '<object type="text/html" data="../test.html" ></object>';
+	document.getElementById("contentContainer").src = filePath;	
+}

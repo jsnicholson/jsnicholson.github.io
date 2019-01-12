0d4926b8d95996557c96747a7bd81b68e141469c
Josh Nicholson <jsnicholson08@gmail.com>
2019-01-12 01:06:14 +0000

    Simpler content loading

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,4 +1,3 @@
-function readTextFile(filePath) {
-	//document.getElementById("contentContainer").innerHTML = '<object type="text/html" data="../test.html" ></object>';
-	document.getElementById("contentContainer").src = filePath;	
+function LoadToDiv(filePath, divId){
+	$(divId).load(filePath);
 }

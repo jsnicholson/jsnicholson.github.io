bca33e8e8b3611fdaf7584c0ac22504fb7be0a81
Josh Nicholson <jonic@netcompany.com>
2022-05-03 15:45:28 +0100

    Added smooth scrolling and added more cv content

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ function LoadHtmlInto(filename, id) {
     .then(response => response.text())
     .then(text => document.getElementById(id).innerHTML = text);
 }
+
+function ScrollTo(id){
+    var element = document.getElementById(id);
+    element.scrollIntoView(true);
+    //$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
+}
+
+function ScrollTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
